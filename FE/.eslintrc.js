283bde1ec1c0921529a438ba4de3405916b6d076
Tran Huy <tdhuy141vt@gmail.com>
2025-12-22 02:07:12 +0700

    add ESLint configuration, update package

---
+++ FE/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: ["eslint:recommended", "plugin:react/recommended"],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["react", "prettier"],
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+    rules: {
+        // "linebreak-style": ["error", process.platform === "win32" ? "windows" : "unix"],
+        "linebreak-style": 0,
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+        "no-unused-vars": "off",
+        "no-console": "off",
+        "no-prototype-builtins": "off",
+        "no-empty": "warn",
+        "react/prop-types": "off",
+    },
+};
